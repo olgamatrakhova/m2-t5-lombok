fb0aa3d3c088530daf7eb5d9347648c0b5873ce1
olgamatrakhova <gluhova.olga@gmail.com>
2023-08-10 12:47:40 +0300

    тринировка

--- src/main/java/ru/yandex/practicum/Point.java
+++ src/main/java/ru/yandex/practicum/Point.java
@@ -1,10 +1,14 @@
 package ru.yandex.practicum;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode
 public class Point {
     private int x;
     private int y;
